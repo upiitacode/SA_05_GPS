1fce39e9e4d83f240134e5991c4c5c05b958664f
Manuel Limones <limonesu.me@gmail.com>
2015-11-06 02:40:39 -0600

    New tokenizer with tests

--- matlab/tokenizeLine.m
+++ matlab/tokenizeLine.m
@@ -1,5 +1,5 @@
 function [ tokens ] = tokenizeLine( line )
 %UNTITLED Summary of this function goes here
 %   Detailed explanat
-tokens = regexp(line,'(\w+)\s*=\s*(+?-?\d+\.?\d*)','tokens');
+tokens = regexp(line,'\$(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(\w+)\*(\w+)','tokens');
 end
--- matlab/tokenizeLine_test.m
+++ matlab/tokenizeLine_test.m
@@ -7,22 +7,10 @@ result =tokenizeLine('');
 assert(isempty(result));
 assert(iscell(result));
 %% Test 2: that well formated string is parsed to a cell
-parsed_cell =tokenizeLine('ax = 2.5');
-expected_cell = {{'ax','2.5'}};
-assert(isequal(parsed_cell,expected_cell));
-%% Test 3: that wrong formated string is parsed to empty cell
-parsed_cell =tokenizeLine('ax = ');
-assert(isempty(parsed_cell));
-assert(iscell(parsed_cell));
-%% Test 4: that multimpe tokes are parsed to nested cell
-parsed_cell =tokenizeLine('ax = 2.5, ay = 4');
-expected_cell = {{'ax','2.5'},{'ay','4'}};
-assert(isequal(parsed_cell,expected_cell));
-%% Test 5: that bad formatted token is inogoren in multitoken string
-parsed_cell =tokenizeLine('ax = 2.5, bd = , az = 2654');
-expected_cell = {{'ax','2.5'},{'az','2654'}};
-assert(isequal(parsed_cell,expected_cell));
-%% Test 6: that token can ve negative number
-parsed_cell =tokenizeLine('ax = -2.5');
-expected_cell = {{'ax','-2.5'}};
-assert(isequal(parsed_cell,expected_cell));
+parsed_cell =tokenizeLine('$GPRMC,075013.25,A,1927.79076,N,09907.64324,W,0.477,,061115,,,A*61');
+expected_cell = {'GPRMC','075013.25','A','1927.79076','N','09907.64324','W','0.477','','061115','','','A','61'};
+assert(isequal(parsed_cell{1},expected_cell));
+%% Test 3: that well formated string is parsed to a cell
+parsed_cell =tokenizeLine('$GPRMC,082906.00,V,,,,,,,061115,,,N*7A');
+expected_cell = {'GPRMC','082906.00','V','','','','','','','061115','','','N','7A'};
+assert(isequal(parsed_cell{1},expected_cell));
