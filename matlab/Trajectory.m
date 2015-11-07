5501351b310b98d028fa1b162780c8ff80c82670
Manuel Limones <limonesu.me@gmail.com>
2015-11-07 05:04:49 -0600

    complete trajectory class with tests

--- matlab/Trajectory.m
+++ matlab/Trajectory.m
@@ -7,8 +7,9 @@ classdef Trajectory < handle
         positionTolerance;
         next_checkpoint = 2;
         last_checkpoint = 1;
-        last_position = 0.5;
-        current_position = 0.5;
+        last_position = 0;
+        current_position = 0;
+        isComplete = false;
     end
     
     methods
@@ -21,39 +22,56 @@ classdef Trajectory < handle
         end
 
         function [outOfCourse] = isOutOfCourse(obj)
-            outOfCourse  = false;
+            currentCompletion = obj.getSegmentCompletion();
+            lastCompletion = obj.getUnitaryCompletion(obj.last_position);
+            outOfCourse =  lastCompletion > currentCompletion;
         end
-
-        function [len] = getTotalLength(obj)
+        
+        function [len] = getLengthUpToCheckpoint(obj, checkpoint)
             len  = 0;
-            for i = [1 : (length(obj.pointList) - 1)]
+            for i = [1 : (checkpoint -1)]  
                 len  = len + norm(obj.pointList{i} - obj.pointList{i+1});
             end
+        end
+        function [len] = getTotalLength(obj)
+             len = obj.getLengthUpToCheckpoint(length(obj.pointList));
         end
 
         function [len] = getSegmentLength(obj)
             len  = norm(obj.pointList{obj.last_checkpoint} - obj.pointList{obj.next_checkpoint});
         end
 
-        function [completion] = getSegmentCompletion(obj)
-            completion  = 0;
+        function [unitaryCompletion] = getSegmentCompletion(obj)
+           unitaryCompletion = obj.getUnitaryCompletion(obj.current_position);
     	end
 
-        function [completion] = getTotalCompletion(obj)
-            completion  = 0;
+        function [unitaryCompletion] = getTotalCompletion(obj)
+             unitarySegmentCompletion = obj.getUnitaryCompletion(obj.current_position);
+             segmentLength = obj.getSegmentLength();
+             segmentCompletion = unitarySegmentCompletion*segmentLength();
+             completion  = segmentCompletion + obj.getLengthUpToCheckpoint(obj.last_checkpoint);
+             unitaryCompletion = completion/obj.getTotalLength();
         end
 
         function [error] = getNormalError(obj)
-            error  = 0;
+             Ra = obj.pointList{obj.last_checkpoint}';
+            Rb = obj.pointList{obj.next_checkpoint}';
+            Rc = obj.current_position';
+            [~, error] = getProgressAndError(Ra,Rb,Rc);
         end
 
         function setPosition(obj,position)
+            obj.last_position = obj.current_position;
             obj.current_position = position;
-	    nextCP_position = obj.pointList{obj.next_checkpoint};
-	    if (norm(nextCP_position - position) < obj.positionTolerance)
-	    	obj.next_checkpoint = obj.next_checkpoint + 1;
-	    	obj.last_checkpoint = obj.last_checkpoint + 1;
-	    end
+            nextCP_position = obj.pointList{obj.next_checkpoint};
+            if (norm(nextCP_position - position) < obj.positionTolerance)
+                if (obj.next_checkpoint < length(obj.pointList))
+                    obj.next_checkpoint = obj.next_checkpoint + 1;
+                    obj.last_checkpoint = obj.last_checkpoint + 1;
+                else
+                    obj.isComplete = true;
+                end
+            end
         end
 
         function [position] = getPosition(obj)
@@ -63,7 +81,26 @@ classdef Trajectory < handle
         function [index]  = getNextCheckpointIndex(obj)
             index = obj.next_checkpoint;
         end
-
+        
+        function [unitaryCompletion] = getUnitaryCompletion(obj, position)
+            Ra = obj.pointList{obj.last_checkpoint}';
+            Rb = obj.pointList{obj.next_checkpoint}';
+            Rc = position';
+            completion  = getProgressAndError(Ra,Rb,Rc);
+            unitaryCompletion = completion/obj.getSegmentLength(); 
         end
 
+        end
+    
+end
+
+function [progress, error] = getProgressAndError(Ra,Rb,Rc)
+U = (Rb - Ra);
+rotationMatrix = [0 -1; 1 0];
+u = U/norm(U);
+uR = rotationMatrix*u;
+B = [u uR];
+k = inv(B)*(Rc - Ra);
+progress = k(1);
+error = k(2);
 end
--- matlab/Trajectory_test.m
+++ matlab/Trajectory_test.m
@@ -15,6 +15,7 @@ assert(isequal(completion, 0));
 
 outOfCourse = trajectory.isOutOfCourse();
 assert(isequal(outOfCourse, false));
+assert(~trajectory.isComplete);
 %% Test 1: Position is set on creation
 trajectory  = Trajectory({[4 5]},[1 1],0.2);
 position = trajectory.getPosition();
@@ -62,4 +63,50 @@ assert(isequal(nCheckpoint, 4));
 
 sLength = trajectory.getSegmentLength();
 assert(isequal(sLength, 10));
-%%
+%% Tests 4: Segment Completion can be calculated
+trajectory  = Trajectory({[3 3],[8 8]},[1 1],0.2);
+trajectory.setPosition([2 2]);
+completion = trajectory.getSegmentCompletion();
+assert(isequal(completion, 0.5));
+error = trajectory.getNormalError();
+assert(isequal(error, 0));
+%% Tests 5: Trajectory error can be calculated
+trajectory  = Trajectory({[3 3],[8 8]},[1 1],0.2);
+trajectory.setPosition([1 2]);
+error = trajectory.getNormalError();
+assert(abs(error - sqrt(1/2)) < 0.0001);
+%% Tests 6: can tell if out of course
+trajectory  = Trajectory({[3 3],[8 8]},[1 1],0.2);
+outOfCourse = trajectory.isOutOfCourse();
+assert(isequal(outOfCourse, false));
+
+trajectory.setPosition([2 2]);
+outOfCourse = trajectory.isOutOfCourse();
+assert(isequal(outOfCourse, false));
+
+trajectory.setPosition([1.5 1.5]);
+outOfCourse = trajectory.isOutOfCourse();
+assert(isequal(outOfCourse, true));
+
+trajectory.setPosition([2.5 2.5]);
+outOfCourse = trajectory.isOutOfCourse();
+assert(isequal(outOfCourse, false));
+%% Tests 7 : total completion can be calculated
+trajectory  = Trajectory({[3 3],[1 5]},[1 1],0.2);
+trajectory.setPosition([2 2]);
+completion = trajectory.getTotalCompletion();
+assert(isequal(completion, 0.25));
+
+trajectory.setPosition([3 3]);
+completion = trajectory.getTotalCompletion();
+assert(isequal(completion, 0.50));
+
+trajectory.setPosition([2 4]);
+completion = trajectory.getTotalCompletion();
+assert(isequal(completion, 0.75));
+
+trajectory.setPosition([1 5]);
+completion = trajectory.getTotalCompletion();
+assert(isequal(completion, 1));
+
+assert(trajectory.isComplete);
