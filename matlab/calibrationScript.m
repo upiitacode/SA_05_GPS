52a3fd2fae4a6125e515d1a344171103a8f33ef2
Manuel Limones <limonesu.me@gmail.com>
2015-11-10 04:43:10 -0600

    Add calibration script

---
+++ matlab/calibrationScript.m
@@ -0,0 +1,33 @@
+clear all
+gps = MockGPS();
+
+disp('Select 3 points in map')
+yellow = uint8([255 255 0]); % [R G B]; class of yellow must match class of I
+blue = uint8([0 0 255]); % [R G B]; class of yellow must match class of IG
+green= uint8([0 255 0]); % [R G B]; class of yellow must match class of I
+
+yellowCirclePainter = vision.ShapeInserter('Shape','Circles','BorderColor','Custom','CustomBorderColor',yellow,'LineWidth',5);
+blueCirclePainter = vision.ShapeInserter('Shape','Circles','BorderColor','Custom','CustomBorderColor',blue,'LineWidth',5);
+greenCirclePainter = vision.ShapeInserter('Shape','Circles','BorderColor','Custom','CustomBorderColor',green,'LineWidth',5);
+
+I = imread('upiita_02.png');
+axes(gca);
+imshow(I); 
+[x , y] = getpts(gca);
+P = [x,y]';
+for i = 1:3
+    readString = input('Go to circle, Press any key to continue\n');
+    stablePos = gps.readStableData(10);
+    Q(:,i) = [stablePos.longitude; stablePos.latitude];
+end
+%%
+[Apq Bpq] = MapTranslator.getMatrix(P,Q)
+[Aqp Bqp] = MapTranslator.getMatrix(Q,P)
+%% Coordinate to pixel
+q = [-99.12787310779095,19.512205595703424]'; 
+p =Aqp * q + Bqp; 
+pt = p'
+points = [pt, ones(size(pt,1),1)*10]
+points = int32(points)
+K = step(blueCirclePainter, I, points);
+imshow(K); 
